98f279a5d4bea012df30e86b2c4eda05c1c07ac8
Mr. Mendez <mrmendez@amvestor.com>
2023-12-06 01:07:13 -0500

    Remove unnecessary code and update MyStack component

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,21 +9,12 @@ import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
   integrations: [
     sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
-      ],
-    }),
     solidJs(),
     UnoCSS({ injectReset: true }),
   ],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
-  output: "server",
-  adapter: netlify(),
 });
